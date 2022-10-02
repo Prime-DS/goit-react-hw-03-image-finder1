e8b48dad4768de4853b9928b3ec2798f2d7c11c3
Prime-DS <prime.up17@gmail.com>
2022-10-02 14:39:53 +0300

    fix featch

--- src/components/api/post.js
+++ src/components/api/post.js
@@ -11,50 +11,13 @@ const instance = axios.create({
     orientation: 'horizontal',
   },
 });
-export const getImage = async (_page = 1) => {
-  const { data } = await instance.get('/', {
-    params: {
-      _page,
-    },
-  });
-  return data;
-};
 
-export const searchImages = async (q, _page = 1) => {
+export const searchImages = async (q, page) => {
   const { data } = await instance.get('/', {
     params: {
-      _page,
+      page,
       q,
     },
   });
   return data;
 };
-
-// const KEY_API = '29398467-8a653d7b4fed816ab704a6050';
-// export const searchImages = async (q, page = 1) => {
-//   try {
-//     const response = await axios.get(URL, {
-//       params: {
-//         key: KEY_API,
-//         q: q,
-//         image_type: 'photo',
-//         orientation: 'horizontal',
-//         per_page: LIMIT,
-//         page: page,
-//       },
-//     });
-//     return data;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-// const instance = axios.create({
-//   baseURL: URL,
-
-//   params: {
-//     per_page: LIMIT,
-//     key: '29398467-8a653d7b4fed816ab704a6050',
-//     image_type: 'photo',
-//     orientation: 'horizontal',
-//   },
-// });
